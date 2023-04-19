873d694a9e18dd129645ebad185d80ce02ae680b
Bogi15 <87603630+Bogi15@users.noreply.github.com>
2023-04-19 19:25:09 +0300

    day 2

---
+++ Leetcode Daily/19-04-2023.java
@@ -0,0 +1,15 @@
+class Solution {
+      public int[] constructRectangle(int area) {
+      int[] arr = new int[2];
+      int Diff = 10_000_001;
+      for (int L = 1; L <= area; L++) {
+         int W = (area / L);
+         if (L * W == area && L >= W && L - W < Diff) {
+            arr[0] = L;
+            arr[1] = W;
+            Diff = L - W;
+        }
+    }
+    return arr;
+  }
+}
