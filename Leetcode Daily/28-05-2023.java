557052d27a8845bee1e3cc65d6abeb3a37f47bf3
Bogi15 <87603630+Bogi15@users.noreply.github.com>
2023-05-28 22:22:29 +0300

    day 34
    34

---
+++ Leetcode Daily/28-05-2023.java
@@ -0,0 +1,27 @@
+//leetcode 2149
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        Queue<Integer> pos = new LinkedList<>();
+        Queue<Integer> neg = new LinkedList<>();
+        int ans[] = new int [nums.length];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                pos.add(nums[i]);
+            }
+            if(nums[i]<0){
+                neg.add(nums[i]);
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+            if(i%2==0) {
+                ans[i] = pos.peek();
+                pos.remove();
+            }
+            if(i%2==1){
+                ans[i] = neg.peek();
+                neg.remove();
+            }
+        }
+        return ans;
+    }
+}
