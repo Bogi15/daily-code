86c6463f9fc6883907b4f1e413d9b0ca4de26b7e
Bogi15 <87603630+Bogi15@users.noreply.github.com>
2023-05-30 22:12:03 +0300

    day 27.1

---
+++ Leetcode Daily/30-05-2023_1.java
@@ -0,0 +1,18 @@
+//leetcode 125
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        String s1 = ""; 
+        String s2 = ""; 
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(Character.isLetterOrDigit(c)) s1 = s1 + c;
+        }
+        for (int i=0; i<s1.length(); i++){
+            char c = s1.charAt(i); 
+            s2 = c+s2; 
+        }
+        if(s1.equals(s2)) return true;
+        else return false;
+    }
+}
