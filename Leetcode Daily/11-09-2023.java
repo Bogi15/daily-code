00b37526285f879291ad0325730a000e26cd6d6b
Bogomil Pankov <87603630+Bogi15@users.noreply.github.com>
2023-09-11 16:13:50 +0300

    day 125

---
+++ Leetcode Daily/11-09-2023.java
@@ -0,0 +1,12 @@
+//leetcode 2357 
+class Solution {
+    public int minimumOperations(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                set.add(nums[i]);
+            }
+        }
+        return set.size();
+    }
+}
